ecbf4e34686f15bf59ab315f712e450d2c0388bb
moetaz <3ezo.yasser@gmail.com>
2024-12-19 06:12:10 -0500

    trial

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,8 +40,8 @@ function NavBar() {
       <Container>
         {/* <Navbar.Brand href="/" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
-        <Navbar.Toggle
+        </Navbar.Brand> */}
+         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
             updateExpanded(expand ? false : "expanded");
@@ -50,7 +50,7 @@ function NavBar() {
           <span></span>
           <span></span>
           <span></span>
-        </Navbar.Toggle> */}
+        </Navbar.Toggle>
         
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav style = {{justifySelf: "center"}} defaultActiveKey="#home">
